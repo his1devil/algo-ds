a920b190a16126f47da073a6f632eba60b063187
root <fengantai@xidibuy.com>
2016-03-12 19:05:45 +0800

    updated

---
+++ trees/binaryTree.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 构造一个root node和两个空子树
+# Binary Tree
+def BinaryTree(r):
+    return [r, [], []]
+
+def insertLeft(root, newBranch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1, [newBranch, t, []])
+    else:
+        root.insert(1, [newBranch, [], []])
+
+    return root
+
+def insertRight(root, newBranch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2, [newBranch, [], t])
+    else:
+        root.insert(2, [newBranch, [], []])
+
+    return root
+
+def getRootVal(root):
+    return root[0]
+
+def setRootVal(root, newVal):
+    root[0] = newVal
+
+def getLeftChild(root):
+    return root[1]
+
+def getRightChild(root):
+    return root[2]
+
+r = BinaryTree(3)
+
+insertLeft(r, 4)
+insertLeft(r, 5)
+insertRight(r, 6)
+insertRight(r, 7)
+print r
+l = getLeftChild(r)
+print l
